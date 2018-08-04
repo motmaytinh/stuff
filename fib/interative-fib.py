39732b9babb5c6fb32b80ecc020511f2ab0389e1
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-08-04 20:45:18 +0700

    Add iterative-fib and reformat using auto pep8

--- fib/interative-fib.py
+++ fib/interative-fib.py
@@ -1,10 +1,17 @@
 import argparse
 
 
-def recursive_fib(n):
+def iterative_fib(n):
+    f1 = 1
+    f2 = 1
     if n <= 2:
-        return 1
-    return recursive_fib(n-2) + recursive_fib(n-1)
+        return f1
+    f3 = f2 + f1
+    for n in range(3, n, 1):
+        f1 = f2
+        f2 = f3
+        f3 = f2 + f1
+    return f3
 
 
 if __name__ == "__main__":
@@ -13,4 +20,4 @@ if __name__ == "__main__":
     parser.add_argument('integers', metavar='N', type=int,
                         default=10, help='The nth fibonacci.')
     args = parser.parse_args()
-    print(recursive_fib(args.integers))
+    print(iterative_fib(args.integers))
