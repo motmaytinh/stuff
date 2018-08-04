fac2765f06da5db9a68f1b0721675fe21deee2c3
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-08-04 20:48:37 +0700

    Rewrite iterative-fib the pythonic way

--- fib/interative-fib.py
+++ fib/interative-fib.py
@@ -2,16 +2,10 @@ import argparse
 
 
 def iterative_fib(n):
-    f1 = 1
-    f2 = 1
-    if n <= 2:
-        return f1
-    f3 = f2 + f1
-    for n in range(3, n, 1):
-        f1 = f2
-        f2 = f3
-        f3 = f2 + f1
-    return f3
+    a, b = 0, 1
+    for i in range(0, n):
+        a, b = b, a + b
+    return a
 
 
 if __name__ == "__main__":
